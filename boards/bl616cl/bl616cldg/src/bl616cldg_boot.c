8997e188714c705678dc58ce0f50070b3c8cf904
SunJ <jsun@bouffalolab.com>
2026-07-09 15:58:38 +0800

    chore(bl616cl): split board boot hooks
    1. Fixed boot hooks mixed with app init
    2. Fixed by adding bl616cldg_boot.c
    3. This keeps appinit for app hooks
    
    Signed-off-by: SunJ <jsun@bouffalolab.com>

---
+++ boards/bl616cl/bl616cldg/src/bl616cldg_boot.c
@@ -0,0 +1,62 @@
+/****************************************************************************
+ * apps/vendor/bouffalolab/boards/bl616cl/bl616cldg/src/bl616cldg_boot.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include "bl616cldg.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_early_initialize
+ *
+ * Description:
+ *   This hook runs after up_initialize() on the startup thread. Keep it for
+ *   board-specific early hardware that is safe before the bring-up thread.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_BOARD_EARLY_INITIALIZE
+void board_early_initialize(void)
+{
+}
+#endif
+
+/****************************************************************************
+ * Name: board_late_initialize
+ *
+ * Description:
+ *   This hook runs in the AppBringUp thread and starts BL616CLDG board
+ *   initialization that may depend on scheduler context.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_BOARD_LATE_INITIALIZE
+void board_late_initialize(void)
+{
+  bl616cldg_bringup();
+}
+#endif
